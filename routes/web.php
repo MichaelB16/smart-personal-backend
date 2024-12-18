7066be8d62bc3b5990384bb3f1e908f3f6e43254
Michael <michael.sousa@terceirizados.farmarcas.com.br>
2024-12-17 23:02:50 -0300

    feat: rota de migrate

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/run-migrate', function() {
+    Artisan::call('migrate');
+    return 'Migrations executed successfully!';
 });
