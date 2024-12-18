18b03d3e98f06c2d5f395db28f492b2a0d7de1fc
Michael <michael.sousa@terceirizados.farmarcas.com.br>
2024-12-18 11:16:32 -0300

    feat: ajuste layout backend

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::get('/run-migrate', function() {
-    Artisan::call('migrate');
-    return 'Migrations executed successfully!';
-});
