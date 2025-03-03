b0c824f9d402385f96fab584be09912041e75895
Michael <michael.sousa@terceirizados.farmarcas.com.br>
2025-03-03 16:25:59 -0300

    feat: ajsutando versão do gemini

--- app/Services/GeminiAiService.php
+++ app/Services/GeminiAiService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Enums\TYPE_FORMAT;
 use Gemini\Data\GenerationConfig;
+use Gemini\Enums\ModelType;
 use Gemini\Laravel\Facades\Gemini;
 
 
@@ -12,8 +13,9 @@ class GeminiAiService
 
     public function sendMessage(string $prompt, string $format = TYPE_FORMAT::TEXT->value)
     {
+        $gemini = Gemini::generativeModel(ModelType::GEMINI_FLASH)->withGenerationConfig(new GenerationConfig());
 
-        $response = Gemini::geminiPro()->withGenerationConfig(new GenerationConfig())->generateContent($prompt);
+        $response = $gemini->generateContent($prompt);
 
         if ($format === TYPE_FORMAT::JSON->value) {
 
