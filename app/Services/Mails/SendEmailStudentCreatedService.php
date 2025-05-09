23536bba78dd37f523775029d008d4b68441112e
Michael <michael.sousa@terceirizados.farmarcas.com.br>
2025-05-08 22:15:26 -0300

    feat: implementando envio via pipedream

--- app/Services/Mails/SendEmailStudentCreatedService.php
+++ app/Services/Mails/SendEmailStudentCreatedService.php
@@ -4,15 +4,30 @@ namespace App\Services\Mails;
 
 use App\Contracts\SendEmailInterface;
 use App\Jobs\SendStudentCreatedEmail;
+use App\Services\NewPasswordService;
+use Exception;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class SendEmailStudentCreatedService implements SendEmailInterface
 {
+
+    public function __construct(protected NewPasswordService $newPasswordService) {}
+
     public function send(array $data)
     {
-        SendStudentCreatedEmail::dispatch([
-            'student_id' => $data['student_id'],
-            'email' => $data['email'],
-            'username' => $data['username'],
-        ]);
+        try {
+            $newPassword = $this->newPasswordService->create([
+                'student_id' => $data['student_id'],
+            ]);
+
+            Http::post(env('WEBHOOK_PIPEDREAM_URL'), [
+                ...$data,
+                'event' => 'student_created',
+                'url' => env('APP_URL_FRONT') . '/new/password/' . $newPassword->token
+            ]);
+        } catch (Exception $e) {
+            Log::error('ERROR SEND EMAIL CREATE STUDENT: ' . $e->getMessage());
+        }
     }
 }
